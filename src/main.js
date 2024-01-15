4b88e263be35efe637f1e37c0597fbe3e8c35267
贺创 <2236402130@qq.com>
2024-01-15 17:43:53 +0800

    第一课（抽离函数）

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 const TEXT_NODE = 'react_text_node'
 
-const textVNode = {
-  type: TEXT_NODE,
-  props: {
-    nodeValue: 'app'
-  }
-}
+const textVNode = createNode(TEXT_NODE, { nodeValue: 'app' })
 // 构建虚拟dom
-const vDom = {
-  type: 'div',
-  props: {
-    id: 'app'
-  },
-  children: [textVNode]
-}
+const vDom = createNode('div', { id: 'app' }, textVNode)
 
-
-// 硬编码
-const dom = document.createElement(vDom.type)
-dom.id = vDom.props.id
-document.querySelector('#root').appendChild(dom)
-
-const textNode = createElement(textVNode.type)
-textNode.nodeValue = textVNode.props.nodeValue
-dom.appendChild(textNode)
+render(vDom, document.querySelector('#root'))
 
 function createElement(type) {
   return type === TEXT_NODE
     ? document.createTextNode("")
     : document.createElement(type)
+}
+
+function createNode(type, props, ...children) {
+  return {
+    type,
+    props,
+    children
+  }
+}
+
+function render({ type, props, children }, container) {
+  const el = createElement(type)
+  for (const key in props) {
+    if (key === 'children') continue
+    el[key] = props[key]
+  }
+  container.appendChild(el)
+  if (children.length) children.forEach(child => {
+    render(child, el)
+  })
 }
