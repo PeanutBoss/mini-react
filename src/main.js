a3b1a97de4f1abb44cbe96d1884f4aa432953c7a
贺创 <2236402130@qq.com>
2024-01-15 17:30:27 +0800

    第一课（虚拟dom）

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+const TEXT_NODE = 'react_text_node'
+
+const textVNode = {
+  type: TEXT_NODE,
+  props: {
+    nodeValue: 'app'
+  }
+}
+// 构建虚拟dom
+const vDom = {
+  type: 'div',
+  props: {
+    id: 'app'
+  },
+  children: [textVNode]
+}
+
+
 // 硬编码
 const dom = document.createElement('div')
 dom.id = 'app'
