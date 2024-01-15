a7ab8386b8f467ab7393b2d48770368e492983a3
贺创 <2236402130@qq.com>
2024-01-15 17:17:36 +0800

    第一课（硬编码）

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// 硬编码
+const dom = document.createElement('div')
+dom.id = 'app'
+document.querySelector('#root').appendChild(dom)
+
+const textNode = document.createTextNode("")
+textNode.nodeValue = 'app'
+dom.appendChild(textNode)
