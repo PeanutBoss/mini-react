90a008d20a7f7aba2e5f975e066c02e5d4de5760
贺创 <2236402130@qq.com>
2024-01-16 14:16:38 +0800

    第一课（完成）

--- src/core/React.js
+++ src/core/React.js
@@ -3,19 +3,30 @@ export const TEXT_NODE = 'react_text_node'
 export function createNode(type, props, ...children) {
   return {
     type,
-    props,
-    children
+    props: {
+      ...props,
+      children: children.map(child => typeof child === 'string' ? createTextNode(child) : child)
+    }
+  }
+}
+function createTextNode(text) {
+  return {
+    type: TEXT_NODE,
+    props: {
+      nodeValue: text,
+      children: []
+    }
   }
 }
 
-export function render({ type, props, children }, container) {
+export function render({ type, props }, container) {
   const el = createElement(type)
   for (const key in props) {
     if (key === 'children') continue
     el[key] = props[key]
   }
   container.appendChild(el)
-  if (children.length) children.forEach(child => {
+  if (props.children.length) props.children.forEach(child => {
     render(child, el)
   })
 }
