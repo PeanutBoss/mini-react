3c5194f6ade531f3765c10e98776a915cf317ca6
贺创 <2236402130@qq.com>
2024-01-16 15:05:05 +0800

    第二课（支持标签的写法）

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { createElement, TEXT_NODE } from './core/React.js'
+import * as React from "./core/React";
+// import { createElement, TEXT_NODE } from './core/React.js'
 
-const textVNode = createElement(TEXT_NODE, { nodeValue: 'app' })
-const App = createElement('div', { id: 'app' }, textVNode)
+// const textVNode = createElement(TEXT_NODE, { nodeValue: 'app' })
+// const App = createElement('div', { id: 'app' }, textVNode)
 
+const App = <div>app</div>
 console.log(App, 'App')
 
 export default App
